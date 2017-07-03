8f88808d7655fedab8cc1531eb3b68ef02cdfbcd
Ben Jansen <ben.jansen@oracle.com>
2017-07-03 14:01:31 -0700

    Separate chefspec contexts better and put each assertion in its own test

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -11,21 +11,31 @@ describe 'chefspec-render-file::default' do
     it 'deletes the config file' do
       expect(chef_run).to delete_file(config_path)
     end
+  end
 
-    context 'with the optional attribute set' do
-      let(:setting) {'some_value'}
+  context 'with the optional attribute set' do
+    let(:setting) {'some_value'}
 
-      let(:chef_run) do
-        ChefSpec::SoloRunner.new(platform: 'oracle', version: '7.2') do |node|
-          node.override['chefspec-render-file']['setting'] = setting
-        end.converge(described_recipe)
-      end
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new(platform: 'oracle', version: '7.2') do |node|
+        node.override['chefspec-render-file']['setting'] = setting
+      end.converge(described_recipe)
+    end
 
-      it 'creates the config file' do
-        expect(chef_run).to_not delete_file(config_path)
-        expect(chef_run).to create_template(config_path)
-        expect(chef_run).to render_file(config_path).with_content(/#{setting}/)
-      end
+    it 'does not delete the config file' do
+      expect(chef_run).to_not delete_file(config_path)
+    end
+
+    it 'creates the config file, verified by create_template' do
+      expect(chef_run).to create_template(config_path)
+    end
+
+    it 'creates the config file, verified by render_file' do
+      expect(chef_run).to render_file(config_path)
+    end
+
+    it 'creates the config file, verified by render_file with content' do
+      expect(chef_run).to render_file(config_path).with_content(/#{setting}/)
     end
   end
 end
